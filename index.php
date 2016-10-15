837dabcdb18a9bee4cc9fb2fda7491ec4d9116d6
froxtrel <froxtreldoren@gmail.com>
2016-10-16 03:37:29 +0800

    Base template

---
+++ index.php
@@ -0,0 +1,330 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+  <title>Kamus KD.net</title>
+
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+
+  <div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.8&appId=481185458719244";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+</head>
+<body>
+  <nav class="light-blue lighten-1" role="navigation">
+    <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo"><img width="60px" src="images/kdp1.png"></a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="#">Login</a></li>
+      </ul>
+
+      <ul id="nav-mobile" class="side-nav">
+        <li><a href="#">Login</a></li>
+      </ul>
+      <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+    </div>
+  </nav>
+
+  <div class="container">
+
+  <br>
+
+  <nav>
+    <div class="nav-wrapper">
+      <form>
+        <div class="input-field">
+          <input id="search" type="search" required placeholder="Search for a word..">
+          <label for="search"><i class="material-icons">search</i></label>
+          <i class="material-icons">close</i>
+        </div>
+      </form>
+    </div>
+  </nav>
+
+  <form>
+    <p>
+    <input name="group1" type="radio" id="test1" />
+    <label for="test1">Dusun » Malay</label>
+
+    <input name="group1" type="radio" id="test2" />
+    <label for="test2">Malay » Dusun</label>
+
+  </form>
+  
+  <br>
+  <div class="row">
+
+        <div class="col s12 m4">
+        <br>
+          <div class="row">
+
+            <div class="col s6">
+                <a class="waves-effect waves-light btn modal-trigger" href="#modal1">ADD WORD</a>
+            </div>
+
+            <div class="col s6">
+                <a class="waves-effect waves-light btn modal-trigger" href="#modal1">GIVE IDEA</a>
+            </div>
+
+          </div>
+         
+         <div class="fb-page" data-href="https://www.facebook.com/kdProject2016/" data-tabs="timeline" data-height="250" data-small-header="false" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="false"><blockquote cite="https://www.facebook.com/kdProject2016/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/kdProject2016/">Dusun Dictionary Project</a></blockquote></div>
+
+          
+        </div>
+
+        <div class="col s12 m8">
+          <div class="preview card grey lighten-4">
+            <div class="card-content white-text">
+              <span class="red-text card-title"><b>Welcome to Kamus.net</b></span>
+              <p class="black-text">Kamus.net is the world's largest and most popular English-Indonesian dictionary on the web. Dedicated entirely to the Bahasa Indonesia language Kamus.net provided instant translations to thousands of words, featuring dictionary definitions in English and Indonesia from several respected resources.</p>
+            </div>
+            <div class="card-action">
+              <p>We need you!
+              Help us make Kamus.net the largest human-edited English-Indonesian dictionary on the web!</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+
+        <div class="col s12 m4">
+
+           <div class="card darken-1 borderz">
+            <div class="black-text card-content white-text">
+              <span class="black-text card-title">FRESH | <small> latest terms </small> </span>
+             <table>
+               <tr>
+                 <td class="black-text center">surfing</td>
+                 <td class="red-text center">berselancar</td>
+               </tr>
+                <tr>
+                 <td class="black-text center">nausea</td>
+                 <td class="red-text center">mual</td>
+               </tr>
+                <tr>
+                 <td class="black-text center">glasier</td>
+                 <td class="red-text center">glasier</td>
+               </tr>
+                <tr>
+                 <td class="black-text center">time</td>
+                 <td class="red-text center">kali</td>
+               </tr>
+                <tr>
+                 <td class="black-text center">fool</td>
+                 <td class="red-text center">bodoh</td>
+               </tr>
+             </table>
+            </div>
+          </div>
+
+          <div class="card darken-1 borderz">
+            <div class="black-text card-content white-text">
+              <span class="black-text card-title">THANKS | <small> most active user </small> </span>
+             <table>
+               <tr>
+                 <td class="black-text center">USERNAME</td>
+                 <td class="green-text center">TOTAL WORD</td>
+               </tr>
+                <tr>
+                 <td class="black-text center">nausea</td>
+                 <td class="green-text center">234</td>
+               </tr>
+                <tr>
+                 <td class="black-text center">glasier</td>
+                 <td class="green-text center">444</td>
+               </tr>
+                <tr>
+                 <td class="black-text center">time</td>
+                 <td class="green-text center">121</td>
+               </tr>
+                <tr>
+                 <td class="black-text center">fool</td>
+                 <td class="green-text center">567</td>
+               </tr>
+             </table>
+            </div>
+          </div>
+
+        </div>
+
+
+        <div class="col s12 m8">
+           <div class="card z-depth-2 borderz">
+            <div class="card-content white-text">
+              <span class="black-text card-title"><h3>Semangat</h3></span>
+              <div class="preview grey lighten-4 black-text">
+              <p>fervour
+              excitement, excitation, inflammation, fervor, fervour (noun)  the state of being emotionally aroused and worked up</p>
+              </div>
+            </div>
+            <div class="card-action">
+               <a href="#"><small>Listen »</small></a>
+               <a href="#"><small>More »</small></a>
+            </div>
+          </div>
+
+          <br>
+
+          <div class="card z-depth-2 borderz">
+            <div class="card-content black-text">
+              <span class="card-title black-text">More dictionary terms on Kamus.net:</span>
+              <div class="row">
+                <div class="col s6">
+                  <table class="responsive-table">
+                     <tr>
+                       <td class="black-text ">surfing</td>
+                       <td class="red-text ">berselancar</td>
+                     </tr>
+                      <tr>
+                       <td class="black-text ">nausea</td>
+                       <td class="red-text ">mual</td>
+                     </tr>
+                      <tr>
+                       <td class="black-text ">glasier</td>
+                       <td class="red-text ">glasier</td>
+                     </tr>
+                      <tr>
+                       <td class="black-text ">time</td>
+                       <td class="red-text ">kali</td>
+                     </tr>
+                      <tr>
+                       <td class="black-text ">fool</td>
+                       <td class="red-text ">bodoh</td>
+                     </tr>
+                   </table>
+                </div>
+
+                <div class="col s6">
+                  <table class="responsive-table">
+                     <tr>
+                       <td class="black-text ">surfing</td>
+                       <td class="red-text ">berselancar</td>
+                     </tr>
+                      <tr>
+                       <td class="black-text ">nausea</td>
+                       <td class="red-text ">mual</td>
+                     </tr>
+                      <tr>
+                       <td class="black-text ">glasier</td>
+                       <td class="red-text ">glasier</td>
+                     </tr>
+                      <tr>
+                       <td class="black-text ">time</td>
+                       <td class="red-text ">kali</td>
+                     </tr>
+                      <tr>
+                       <td class="black-text ">fool</td>
+                       <td class="red-text ">bodoh</td>
+                     </tr>
+                   </table>
+                </div>
+
+
+              </div>
+            </div>
+          </div>
+
+          <br>
+
+          <div class="card z-depth-2 borderz">
+            <div class="card-content grey-text">
+              <span class="card-title">Did you know?</span>
+              <p>The only words with three consecutive double letters are bookkeeping and bookkeeper</p>
+            </div>
+          </div>
+
+          <br>
+
+           <div class="card z-depth-2 borderz">
+            <div class="card-content white-text">
+              <span class="black-text card-title"><small>Share your thoughts:</small></span>
+
+              <div class="fb-comments" data-href="http://localhost/kamus/?#!" data-numposts="5"></div>
+            
+            </div>       
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+  <footer class="page-footer light-blue lighten-1">
+    <div class="container">
+      <div class="row">
+        <div class="col l6 s12">
+          <h5 class="white-text"><img src="images/kdp.png"></h5>
+          <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
+
+
+        </div>
+        <div class="col l3 s12">
+          <!-- <h5 class="white-text">Settings</h5> -->
+          <!-- <ul>
+            <li><a class="white-text" href="#!">Link 1</a></li>
+            <li><a class="white-text" href="#!">Link 2</a></li>
+            <li><a class="white-text" href="#!">Link 3</a></li>
+            <li><a class="white-text" href="#!">Link 4</a></li>
+          </ul> -->
+        </div>
+        <div class="col l3 s12">
+         <!--  <h5 class="white-text">Connect</h5> -->
+         <!--  <ul>
+            <li><a class="white-text" href="#!">Link 1</a></li>
+            <li><a class="white-text" href="#!">Link 2</a></li>
+            <li><a class="white-text" href="#!">Link 3</a></li>
+            <li><a class="white-text" href="#!">Link 4</a></li>
+          </ul> -->
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright">
+      <div class="container">
+      Made by <a class="orange-text text-lighten-3" href="http://materializecss.com">KDP 2016</a>
+      </div>
+    </div>
+  </footer>
+ 
+
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="js/materialize.js"></script>
+  <script src="js/init.js"></script>
+
+  </body>
+</html>
+
+<!-- Modal Structure -->
+  <div id="modal1" class="modal bottom-sheet">
+    <div class="modal-content">
+      <h4>Modal Header</h4>
+      <p>A bunch of text</p>
+    </div>
+    <div class="modal-footer">
+      <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
+    </div>
+  </div>
+          
+  <script type="text/javascript">
+    
+    $(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+  });
+         
+
+  </script>
